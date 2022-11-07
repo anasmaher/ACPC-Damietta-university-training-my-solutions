d2e35e2157451008c9712c59577ac1a15a81772f
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-07 22:44:25 +0200

    Update H. Breakfast.cpp

--- Level-1/Qualification Contest/H. Breakfast.cpp
+++ Level-1/Qualification Contest/H. Breakfast.cpp
@@ -1,1 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >>n;
+        deque<int>v(n);
+
+        ll noor=0,sec=0;
+        for(int i=0 ; i<n ; i++) cin >> v[i];
+
+        int sz=v.size();
+        while(sz)
+        {
+            if(sz==1)
+            {
+                if(noor<=sec) noor+=v.front();
+                else sec+=v.front();
+                break;
+            }
+            if(v.front()>v.back())
+            {
+                noor+=v.front();
+                v.pop_front();
+                v.push_back(v.front());
+                v.pop_front();
+                sz--;
+            }
+            else
+            {
+                sec+=v.back();
+                v.pop_back();
+                v.push_front(v.back());
+                v.pop_back();
+                sz--;
+            }
+        }
+        if(noor>=sec) cout << "YES\n"; else cout << "NO\n";
+    }
+}
