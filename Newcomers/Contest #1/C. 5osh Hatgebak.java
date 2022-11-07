612178cb42d71e78f9549fe4100c2042bfef2d08
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-07 21:52:54 +0200

    Create C. 5osh Hatgebak.java

---
+++ Newcomers/Contest #1/C. 5osh Hatgebak.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        long x = in.nextLong(), y = in.nextLong();
+        if (x > y) out.println("NOOR");
+        else if (x < y) out.println("ANAS");
+        else out.println("INCOMING!!!");
+        out.close();
+    }
+
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
