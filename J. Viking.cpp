ebf61373a36ed3d63efce924c5bdd0c4aa66d86a
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-07 20:43:54 +0200

    Create J. Viking.cpp

---
+++ J. Viking.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    ll x,y,z; cin >> x >> y >> z;
+    cout << ((x*3)+(y*3)+z)/3 << "\n";
+}
