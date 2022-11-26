cbe3c287db42b4e96d488a18647c9942bd1c4a70
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-26 15:58:59 +0200

    Update H. Fight with Monsters.cpp

--- Newcomers/Contest 2/H. Fight with Monsters.cpp
+++ Newcomers/Contest 2/H. Fight with Monsters.cpp
@@ -1,1 +1,31 @@
-م
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++) cin >> arr[i];
+    sort(arr,arr+n);
+
+    int res=0;
+    for(int i=1 ; i<n ; i++)
+    {
+        if(arr[i]!=arr[i-1])res++;
+    }
+    cout << ++res << '\n'; // why ++res ? cuz last number won't be counted, trace it and see.
+}
+
+// another solution
+/*
+    int n; cin >> n;
+    set<int>st;
+    int val;
+    while(n--)
+    {
+        cin >> val;
+        st.insert(val);
+    }
+    cout << st.size() << '\n';
+\*
