41c4e271b66ffa21e71ded6c01c2fbdcbaa3528e
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-26 16:51:54 +0200

    Create A. Eng.Spidey.cpp

---
+++ Newcomers/Contest 2/A. Eng.Spidey.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >> n; cout << 180*(n-2) << '\n';
+    }
+}
