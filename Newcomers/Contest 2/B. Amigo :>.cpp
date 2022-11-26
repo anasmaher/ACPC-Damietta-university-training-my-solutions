e0ebb4555c85cdfbd65b4b7eca84f8c1d2315f64
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-26 16:50:13 +0200

    Create B. Amigo :>.cpp

---
+++ Newcomers/Contest 2/B. Amigo :>.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int x,y; cin >> x >> y;
+    if(abs(x-y)==1) cout << "GOOD LUCK AGENT P\n";
+    else cout << "CURSE YOU\n";
+}
