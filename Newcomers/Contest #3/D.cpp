3ddefe4e0eb94237a6f1b9804a399909bcbd2fa8
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2023-01-01 01:14:33 +0200

    Create D.cpp

---
+++ Newcomers/Contest #3/D.cpp
@@ -0,0 +1,12 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse2")
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    
+    ll n; cin >> n; cout << 5;
+}
